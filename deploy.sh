40043377df9a3d89b61fde122eb85fa6f2b685b4
Pho Huynh <shortgiraffe4@gmail.com>
2018-07-16 17:47:29 +0700

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
-git add .
-git commit --allow-empty -m "Push to gh-pages"
-git subtree push --prefix $1 origin gh-pages
